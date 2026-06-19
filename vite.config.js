1344944b6a880b5d316bf865f26d65a31f340523
ManifestaPremium <arysimeao@gmail.com>
2026-06-19 04:53:22 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Manifesta",
+        short_name: "Manifesta",
+        description: "Afirmações, técnicas de manifestação e sabedoria diária",
+        theme_color: "#C9923F",
+        background_color: "#FAF7F2",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
